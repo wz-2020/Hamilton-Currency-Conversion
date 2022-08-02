6403a82de5310e64f6039c788de64caebbdf0763
wz-2020 <69603240+wz-2020@users.noreply.github.com>
2022-08-02 11:16:51 +0100

    Initial Commit

---
+++ Hamilton-Currency-Conversion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hamilton-Currency-Conversion
+//
+//  Created by Wanchun Zhang on 02/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
